a9d34afd1d67e7c64de085e37c7252ded76a2160
palandora <bernd@dotsandlines.io>
2021-06-21 21:23:31 +0200

    updated js

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 class Tree {
-    constructor(stem_height) {
-        this.state = "birth";
-        this.age = 0;
-        this.stem_height = stem_height;
+    constructor() {
+        this.state = "birth"
+        this.age = 0
+        this.tree
+    }
+    createTree(){
+        var tree = document.createElement('div')
+        tree.setAttribute('class', 'tree')
+        tree.innerHTML = `
+        <div class="tree">
+            <div class="crown growing">
+                <div class="head"></div>
+                <div class="sidepanel top"></div>
+                <div class="repeat"></div>
+                <div class="sidepanel bottom"></div>
+            </div>
+            <div class="stem"></div>
+            <div class="base growing"></div>
+        </div>`
+        document.body.appendChild(tree);
+        this.tree = tree
+    }
+    setStem(){
+        const stem_height = Math.floor(Math.random() * 80)
+        this.tree.querySelector('.stem').style.height = stem_height + "px"
     }
   }
 
-const myTree = new Tree(40);
-console.log(myTree.stem_height);
+const myTree = new Tree()
+myTree.createTree()
+myTree.setStem()
