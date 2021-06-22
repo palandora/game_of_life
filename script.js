36e6d6f526bc3f5246c4e7e353529f1a32623265
palandora <bernd@dotsandlines.io>
2021-06-22 21:58:51 +0200

    added grow() to script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ class Tree {
     constructor() {
         this.state = "birth"
         this.age = 0
+        this.stem_height
         this.tree
     }
     createTree(){
-        var tree = document.createElement('div')
+        const tree = document.createElement('div')
         tree.setAttribute('class', 'tree')
         tree.innerHTML = `
-        <div class="tree">
             <div class="crown growing">
                 <div class="head"></div>
                 <div class="sidepanel top"></div>
@@ -16,17 +16,42 @@ class Tree {
                 <div class="sidepanel bottom"></div>
             </div>
             <div class="stem"></div>
-            <div class="base growing"></div>
-        </div>`
-        document.body.appendChild(tree);
+            <div class="base growing"></div>`
+        //tbd where to add div element later on  
+        document.body.appendChild(tree)
+        const stem_height = Math.floor(Math.random() * 120)
+        tree.querySelector('.stem').style.height = stem_height + "px"
         this.tree = tree
     }
-    setStem(){
-        const stem_height = Math.floor(Math.random() * 80)
-        this.tree.querySelector('.stem').style.height = stem_height + "px"
+    grow(){
+        let age = 0
+        const crown = this.tree.querySelector('.crown')
+        const repeat = this.tree.querySelector('.repeat')
+        console.log(repeat)
+        const base = this.tree.querySelector('.base')
+        setInterval(function(){
+            age+=8
+            console.log(age)
+            if(age < 32){
+                repeat.style.height = 60 + age + "px";
+            }else if(age > 32 && age < 64){
+                crown.classList.remove("growing")
+                crown.classList.add("dead")
+                base.classList.remove("growing")
+                base.classList.add("dead")
+            }else if(age > 64){
+                crown.classList.add("growing")
+                crown.classList.remove("dead")
+                base.classList.add("growing")
+                base.classList.remove("dead")
+                repeat.style.height = 60 + "px"
+                age = 0
+            }
+        }, 3000)
     }
   }
 
 const myTree = new Tree()
 myTree.createTree()
-myTree.setStem()
+myTree.grow()
+
