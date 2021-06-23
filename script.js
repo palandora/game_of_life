68da855e26fdbcc474b2e8f930e7f7ca20c3159c
palandora <bernd@dotsandlines.io>
2021-06-23 19:24:14 +0200

    updated grow() , added blom(),  bug fixes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 class Tree {
     constructor() {
-        this.state = "birth"
-        this.age = 0
-        this.stem_height
+        this.state = "default"
+        this.growthSpeed = 40
+        this.age
         this.tree
     }
     createTree(){
@@ -19,39 +19,60 @@ class Tree {
             <div class="base growing"></div>`
         //tbd where to add div element later on  
         document.body.appendChild(tree)
-        const stem_height = Math.floor(Math.random() * 120)
-        tree.querySelector('.stem').style.height = stem_height + "px"
+        this.setStem(tree, 120)
         this.tree = tree
+    }
+    setStem(tree, factor){
+        const stem_height = Math.floor(Math.random() * factor)
+        tree.querySelector('.stem').style.height = stem_height + "px"
+    }
+    bloom(){
+        const crown = this.tree.querySelector(".crown")
+        //Create apples
+        const apple = document.createElement('div')
+        apple.setAttribute('class','apple')
+        crown.appendChild(apple)
+
+        //place apples
+        const height = crown.clientHeight
+        const widht = crown.clientWidth
     }
     grow(){
         let age = 0
         const crown = this.tree.querySelector('.crown')
         const repeat = this.tree.querySelector('.repeat')
-        console.log(repeat)
         const base = this.tree.querySelector('.base')
+        let obj = this
         setInterval(function(){
-            age+=8
-            console.log(age)
-            if(age < 32){
-                repeat.style.height = 60 + age + "px";
-            }else if(age > 32 && age < 64){
+            age+=obj.growthSpeed
+            if(age <= obj.growthSpeed*4){
+                repeat.style.height = 20 + age + "px"
+                obj.state = "growing"
+            }else if(age > obj.growthSpeed*4 && age <= obj.growthSpeed*8){
                 crown.classList.remove("growing")
                 crown.classList.add("dead")
                 base.classList.remove("growing")
                 base.classList.add("dead")
-            }else if(age > 64){
+                obj.state = "dead"
+            }else if(age > obj.growthSpeed*8){
+                this.state = "rebirth"
                 crown.classList.add("growing")
                 crown.classList.remove("dead")
                 base.classList.add("growing")
                 base.classList.remove("dead")
-                repeat.style.height = 60 + "px"
+                repeat.style.height = 20 + "px"
                 age = 0
             }
+            obj.age = age
         }, 3000)
     }
   }
 
 const myTree = new Tree()
 myTree.createTree()
-myTree.grow()
+//myTree.grow()
+myTree.bloom()
+
+//setInterval(function(){console.log(myTree.age, myTree.state)},3000)
+
 
