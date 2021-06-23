2135b0f9a387722d6a5886bc0081d27db65cdddd
palandora <bernd@dotsandlines.io>
2021-06-23 20:17:11 +0200

    updated bloom function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ class Tree {
         this.growthSpeed = 40
         this.age
         this.tree
+        this.isBlooming = true
     }
     createTree(){
         const tree = document.createElement('div')
@@ -26,16 +27,35 @@ class Tree {
         const stem_height = Math.floor(Math.random() * factor)
         tree.querySelector('.stem').style.height = stem_height + "px"
     }
-    bloom(){
+    bloom(state){
+        const appleAmount = 20
         const crown = this.tree.querySelector(".crown")
-        //Create apples
-        const apple = document.createElement('div')
-        apple.setAttribute('class','apple')
-        crown.appendChild(apple)
-
-        //place apples
         const height = crown.clientHeight
-        const widht = crown.clientWidth
+        const width = crown.clientWidth
+        const apples = []
+
+        if(state){
+            for(let i=0; i<appleAmount; i++){
+                const y = Math.floor(Math.random() * (height-30) +15)
+                const x = Math.floor(Math.random() * width)
+    
+                const apple = document.createElement('div')
+                apple.setAttribute('class','apple')
+                apples.push(apple)
+    
+                apple.style.left = x+"px"
+                apple.style.top = y+"px"
+            }
+            apples.forEach(apple => {
+                crown.appendChild(apple)
+            });
+        }else{
+            const apples = crown.querySelectorAll('.apple')
+            apples.forEach(apple => {
+                crown.removeChild(apple)
+            });
+        }
+
     }
     grow(){
         let age = 0
@@ -43,25 +63,32 @@ class Tree {
         const repeat = this.tree.querySelector('.repeat')
         const base = this.tree.querySelector('.base')
         let obj = this
+
         setInterval(function(){
             age+=obj.growthSpeed
             if(age <= obj.growthSpeed*4){
                 repeat.style.height = 20 + age + "px"
                 obj.state = "growing"
             }else if(age > obj.growthSpeed*4 && age <= obj.growthSpeed*8){
-                crown.classList.remove("growing")
-                crown.classList.add("dead")
-                base.classList.remove("growing")
-                base.classList.add("dead")
-                obj.state = "dead"
+                // crown.classList.remove("growing")
+                // crown.classList.add("dead")
+                // base.classList.remove("growing")
+                // base.classList.add("dead")
+                // obj.state = "dead"
+                if(obj.isBlooming){
+                    obj.bloom(true)
+                    obj.isBlooming = false
+                }
             }else if(age > obj.growthSpeed*8){
                 this.state = "rebirth"
+                obj.bloom(false)
                 crown.classList.add("growing")
                 crown.classList.remove("dead")
                 base.classList.add("growing")
                 base.classList.remove("dead")
                 repeat.style.height = 20 + "px"
                 age = 0
+                obj.isBlooming = true 
             }
             obj.age = age
         }, 3000)
@@ -70,8 +97,9 @@ class Tree {
 
 const myTree = new Tree()
 myTree.createTree()
-//myTree.grow()
-myTree.bloom()
+myTree.grow()
+
+
 
 //setInterval(function(){console.log(myTree.age, myTree.state)},3000)
 
